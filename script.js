4ae5cd043dda3c4c13b8eccfb4640b544b57868a
nameless0o0 <murodrush007@gmail.com>
2024-11-16 17:42:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sidebar = document.getElementById('sidebar');
+    const content = document.querySelector('.content');
+    let menuVisible = false;
+    const Title = document.getElementById("title");
+
+    document.addEventListener('mousemove', (event) => {
+        if (event.clientX < 10 && !menuVisible) {
+            sidebar.classList.add('open');
+            content.style.marginLeft = '250px';
+            menuVisible = true;
+        }
+    });
+
+    sidebar.addEventListener('mouseleave', () => {
+        sidebar.classList.remove('open');
+        content.style.marginLeft = '0';
+        menuVisible = false;
+    });
+
+
+    const exploreBtn = document.getElementById('exploreBtn');
+    exploreBtn.addEventListener('click', () => {
+        alert('Исследуй разделы сайта и найдешь множество полезных инструментов!');
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const themeToggle = document.getElementById("themeToggle");
+    const themeIcon = document.getElementById("themeIcon");
+    const titles = document.querySelectorAll(".title"); // Все элементы с классом title
+    const ps = document.querySelectorAll(".card-paragraph");
+
+    // Проверяем состояние темы из localStorage
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+        themeIcon.textContent = "☀️";
+
+        ps.forEach(p => {
+            p.style.color = "white";
+        });
+        // Меняем цвет всех элементов с классом title на белый
+        titles.forEach(title => {
+            title.style.color = "white";
+        });
+    }
+
+    // Переключатель темы
+    themeToggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+
+        if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("theme", "dark");
+            themeIcon.textContent = "☀️";
+
+            // Меняем цвет всех элементов с классом title на белый
+            titles.forEach(title => {
+                title.style.color = "white";
+            });
+            ps.forEach(p => {
+                p.style.color = "white";
+            });
+        } else {
+            localStorage.setItem("theme", "light");
+            themeIcon.textContent = "🌙";
+
+            // Возвращаем цвет по умолчанию
+            titles.forEach(title => {
+                title.style.color = ""; // Убираем стиль
+            });
+            ps.forEach(p => {
+                p.style.color = ""; // Убираем стиль
+            });
+        }
+    });
+});
+
